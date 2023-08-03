c554b27d8178b6ceb9f9daa89acf40b17292421b
Ajaisvraj <ajaisvraj7907@gmail.com>
2023-08-03 12:03:08 +0530

    finished

--- blog/serealizers.py
+++ blog/serealizers.py
@@ -9,14 +9,20 @@ class CategorySerealizer(serializers.ModelSerializer):
         fields = '__all__'
         
         
-
 class BlogsSerealizer(serializers.ModelSerializer):
+    tag_names = serializers.SerializerMethodField()  # Add this line
+
     class Meta:
         model = Blogs
         fields = '__all__'
+
+    def get_tag_names(self, obj):  # Update method name to match field name
+        return list(obj.tags.all().values_list('name', flat=True))
+
         
         
 class TagSerealizer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
+        
